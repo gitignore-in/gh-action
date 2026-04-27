e218ac4e1ee4a9dec3c286bd9b7f5badd826d176
kitsuyui <kitsuyui@kitsuyui.com>
2026-04-27 21:50:21 +0900

    Install gitignore-in for runner platform

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -9,7 +9,6 @@ fi
 
 sed -i.bak \
   -e "s/version=v[0-9][0-9.]*$/version=${version}/" \
-  -e "s/gitignore-in-x86_64-unknown-linux-gnu-v[0-9][0-9.]*\\.tar\\.gz/gitignore-in-x86_64-unknown-linux-gnu-${version}.tar.gz/" \
   action.yml
 
 rm -f action.yml.bak
