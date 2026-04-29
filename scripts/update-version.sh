ec36d220a01760d86fbbc1d5a83350870715cf26
kitsuyui <kitsuyui@kitsuyui.com>
2026-04-29 13:10:28 +0900

    Add shell format check

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 
 version="${1:-${GITIGNORE_IN_VERSION:-}}"
 if [ -z "${version}" ]; then
-  echo "usage: $0 <version>" >&2
-  exit 1
+	echo "usage: $0 <version>" >&2
+	exit 1
 fi
 
 sed -i.bak \
-  -e "s/version=v[0-9][0-9.]*$/version=${version}/" \
-  action.yml
+	-e "s/version=v[0-9][0-9.]*$/version=${version}/" \
+	action.yml
 
 rm -f action.yml.bak
